44cdc04934afd2a4e9836b9794a72ea87f1091e4
gil7788 <gil7788@gmail.com>
2024-02-05 21:13:00 +0200

    Deploy a Container Using Google Cloud Registry

--- environments/setup/create_user_services.sh
+++ environments/setup/create_user_services.sh
@@ -4,6 +4,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 CONFIG_PATH="$SCRIPT_DIR/../config.sh"
 
+# Load environment variables
 echo "Loading environment variables..."
 if [ -f "$CONFIG_PATH" ]; then
     source "$CONFIG_PATH"
@@ -31,23 +32,38 @@ create_and_grant() {
         --role "$role"
 }
 
-# Ensure the Compute Engine API is enabled
-echo "Ensuring Compute Engine API is enabled..."
-gcloud services enable compute.googleapis.com --project "$PROJECT_ID"
+# Additional function to grant multiple roles to a service account
+grant_roles() {
+    local service_account_email=$1
+    local project_id=$2
+    shift 2
+    local roles=("$@")
+
+    for role in "${roles[@]}"; do
+        gcloud projects add-iam-policy-binding "$project_id" \
+            --member "serviceAccount:$service_account_email" \
+            --role "$role"
+    done
+}
+
+# Ensure the Compute Engine, Artifact Registry, and Cloud Run APIs are enabled
+echo "Ensuring required APIs are enabled..."
+gcloud services enable compute.googleapis.com artifactregistry.googleapis.com run.googleapis.com --project "$PROJECT_ID"
 
 # Create Cloud Run service account
 create_and_grant "cloud-run-sa" "Cloud Run Service Account" "roles/run.admin" "$PROJECT_ID"
 
-# Set up Cloud Build service account
+# Ensure Cloud Run service account can pull images from Artifact Registry
+CLOUD_RUN_SA_EMAIL="${PROJECT_ID}@appspot.gserviceaccount.com"
+grant_roles "$CLOUD_RUN_SA_EMAIL" "$PROJECT_ID" "roles/artifactregistry.reader"
+
+# Setup Cloud Build service account
 CLOUD_BUILD_SA="$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')@cloudbuild.gserviceaccount.com"
 
-# Grant Cloud Build service account necessary roles
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-    --member "serviceAccount:$CLOUD_BUILD_SA" \
-    --role "roles/run.admin"
+# Define roles for Cloud Build service account
+ROLES=("roles/run.admin" "roles/iam.serviceAccountUser" "roles/artifactregistry.admin" "roles/storage.admin")
 
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-    --member "serviceAccount:$CLOUD_BUILD_SA" \
-    --role "roles/iam.serviceAccountUser"
+# Grant defined roles to Cloud Build service account
+grant_roles "$CLOUD_BUILD_SA" "$PROJECT_ID" "${ROLES[@]}"
 
 echo -e "Service accounts and permissions are set up.\n\n"
