605e67ad081f9cf3fcf18d19d172c9e7b5e56e38
gil7788 <gil7788@gmail.com>
2024-02-05 17:45:37 +0200

    Group All Required Setup Script Calls Into setup.sh

--- environments/setup/setup.sh
+++ environments/setup/setup.sh
@@ -1,3 +1,17 @@
-chmod +x deploy_function.sh
+#!/bin/bash
 
-./deploy_function.sh
+# [TODO add cmds status checks, and error handling and improve system messages]
+
+# Determine the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Make deploy_function.sh executable and run it
+chmod +x "$SCRIPT_DIR/deploy_functions.sh"
+"$SCRIPT_DIR/deploy_functions.sh"
+
+# Make create_user_services.sh executable and run it
+chmod +x "$SCRIPT_DIR/create_user_services.sh"
+"$SCRIPT_DIR/create_user_services.sh"
+
+echo "Setup complete."
+exit 0
